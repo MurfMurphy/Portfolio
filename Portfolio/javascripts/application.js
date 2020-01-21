5fd321b670bb5e97f5e0718f2be57de4f252a58d
Alec.Murphy <alecmurphypersonal@gmail.com>
2020-01-21 17:25:38 -0500

    lander animation finished

--- Portfolio/javascripts/application.js
+++ Portfolio/javascripts/application.js
@@ -1,4 +1,12 @@
-document.getElementsByTagName('video')[0].onended = function (){
-    this.load();
-    this.play();
-};
+// document.getElementsByTagName('video')[0].onended = function (){
+//     this.load();
+//     this.play();
+// };
+
+
+// const logo = document.querySelectorAll("#subSVG path");
+
+
+// for (let i = 0; i<logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }
