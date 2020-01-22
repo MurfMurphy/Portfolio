00d529c4caa01a58559992b3e2bd92f81ca33ea5
Alec.Murphy <alecmurphypersonal@gmail.com>
2020-01-22 16:04:58 -0500

    font update/main update

--- Portfolio/javascripts/application.js
+++ Portfolio/javascripts/application.js
@@ -1,12 +1,12 @@
-// document.getElementsByTagName('video')[0].onended = function (){
-//     this.load();
-//     this.play();
-// };
 
-
+//test function for SVG mapping
 // const logo = document.querySelectorAll("#subSVG path");
 
 
 // for (let i = 0; i<logo.length; i++) {
 //     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 // }
+
+if (window.innerWidth < 700){
+    window.location.replace("main.html")
+}
